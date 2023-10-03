8423e8db8b644c9e16a03f65041a7a273d601eda
Igor Zvyagin <spirit-drive@yandex.ru>
2023-10-03 21:09:34 +0300

    fix: set user for all main routing

--- src/rest/routes/mainRouter.ts
+++ src/rest/routes/mainRouter.ts
@@ -6,18 +6,21 @@ import { productRouter, protectedProductRouter } from '../product/productRouter'
 import { operationRouter, protectedOperationRouter } from '../operation/operationRouter';
 import { orderRouter, protectedOrderRouter } from '../order/orderRouter';
 import { uploadRouter } from './uploadRouter';
-import { authentication } from '../authentication';
+import { setUser, authentication } from '../setUser';
 
 export const mainRouter = Router();
+export const routerWithUser = Router();
+
+routerWithUser.use('/profile', authentication, profileRouter);
+routerWithUser.use('/categories', categoryRouter);
+routerWithUser.use('/categories', authentication, protectedCategoryRouter);
+routerWithUser.use('/products', productRouter);
+routerWithUser.use('/products', authentication, protectedProductRouter);
+routerWithUser.use('/operations', operationRouter);
+routerWithUser.use('/operations', authentication, protectedOperationRouter);
+routerWithUser.use('/orders', orderRouter);
+routerWithUser.use('/orders', authentication, protectedOrderRouter);
+routerWithUser.use('/upload', authentication, uploadRouter);
 
 mainRouter.use(authRouter);
-mainRouter.use('/profile', authentication, profileRouter);
-mainRouter.use('/categories', categoryRouter);
-mainRouter.use('/categories', authentication, protectedCategoryRouter);
-mainRouter.use('/products', productRouter);
-mainRouter.use('/products', authentication, protectedProductRouter);
-mainRouter.use('/operations', operationRouter);
-mainRouter.use('/operations', authentication, protectedOperationRouter);
-mainRouter.use('/orders', orderRouter);
-mainRouter.use('/orders', authentication, protectedOrderRouter);
-mainRouter.use('/upload', authentication, uploadRouter);
+mainRouter.use(setUser, routerWithUser);
--- src/rest/setUser.ts
+++ src/rest/setUser.ts
@@ -5,9 +5,15 @@ import { getToken } from '../utils/authentication';
 import { AuthError, NotFoundError } from '../Errors';
 
 export const authentication: RequestHandler = async (req, res, next) => {
+  if (!req.user) return res.status(401).json(new AuthError(`token is required`));
+
+  next();
+};
+
+export const setUser: RequestHandler = async (req, res, next) => {
   const { authorization } = req.headers as { authorization: string };
   const token = getToken(authorization);
-  if (!token) return res.status(401).json(new AuthError(`token is required`));
+  if (!token) return next();
 
   try {
     const { id: userId } = await verify<{ id: string }>(token);
